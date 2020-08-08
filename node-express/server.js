97eeb6c223194d0aaed5fe31d22bc93c673f2b2f
Mohammad Amiri <mohmd.a.a@gmail.com>
2020-08-08 12:33:26 -0700

    Week 1 Workshop Assignment

--- node-express/server.js
+++ node-express/server.js
@@ -1,29 +1,49 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const campsiteRouter = require('./routes/campsiteRouter');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
 
-
-
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use('/campsites', campsiteRouter);
 
+app.use("/campsites", campsiteRouter);
 
+app.all("/campsites", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  next();
+});
 
+app.use("/promotions", promotionRouter);
 
-app.use(express.static(__dirname + '/public'));
+app.all("/promotions", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  next();
+});
+
+app.use("/partners", partnerRouter);
+
+app.all("/partners", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  next();
+});
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
