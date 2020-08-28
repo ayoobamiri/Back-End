d2fc7b2770d70988a161377187d3d63ece1bbf3a
Mohammad Amiri <mohmd.a.a@gmail.com>
2020-08-28 09:56:05 -0700

    Passport Facebook

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "332256577823939",
+    clientSecret: "457084e5aab160488f9c5cdfe31f2d29",
+  },
 };
